416063aa54dd7f385f37ad08e103a649d78f4697
V1bin <maxplayz146@gmail.com>
2026-03-16 10:34:53 +0200

    Renamed files correctly.

--- Date.cpp
+++ Date.cpp
@@ -1,243 +1,289 @@
-#include"Library.h"
+#include "Library.h"
 
-void input();
-
-class RatNum{
-    int numerator;
-    int denominator;
-public:
-    RatNum():numerator(1), denominator(1){}
-    RatNum(int, int);
-    int num(){return numerator;}
-    int den(){return denominator;}
-    void print_d();
-    void print_r();
+enum Month {
+    jan=1, feb, mar, apr, may, jun, jul, aug, sep, octo, nov, dece
 };
 
-RatNum::RatNum(int num, int den):numerator(num)
-{
-    if (den == 0) error("Can't Divide by zero! Try Again...");
-    denominator = den;
-}
+enum Dd {
+    sun = 1, mon, tue, wen, thu, fri, sat = 0
+};
 
-void RatNum::print_d()
-{
-    double result = (double)numerator/denominator;
-    cout << endl << "The result of the rational number is:" << endl;
-    cout << result << endl;
-}
+class Date {
+public:
+    Date(int yy, Month mm, int dd);
 
-void RatNum::print_r()
-{
-    cout << endl << "Your Rational Number is:" << endl;
-    cout << numerator << endl
-         << "--" << endl
-         << denominator << endl;
-}
+    int   day()   {return d;}
+    Month month() {return m;}
+    int   year()  {return y;}
+    Dd   day_name();
 
-RatNum operator+(RatNum& num1, RatNum& num2)
+    bool leap_year();
+    int  month_code(int month);
+    void add_day(int n);
+    void add_month(int n);
+    void add_year(int n);
+    void next_workday();
+    void week_of_year();
+    bool has_30d();
+    bool has_31d();
+private:
+    int   y;
+    Month m;
+    int   d;
+};
+
+bool Date::has_30d()
 {
-    if (num1.den() == num2.den())
+    if (m == apr || m == jun || m == sep || m == nov)
     {
-        int nnum = num1.num() + num2.num();
-        RatNum result(nnum, num1.den());
-        return result;
+        return true;
     }
-    else 
-    {
-        int nnum = (num1.num() * num2.den()) + (num2.num() * num1.den());
-        int nden = (num1.den() * num2.den());
-        RatNum result(nnum, nden);
-        return result;
-    }
-}
-
-RatNum operator-(RatNum& num1, RatNum& num2)
-{
-    if (num1.den() == num2.den())
-    {
-        int nnum = num1.num() - num2.num();
-        RatNum result(nnum, num1.den());
-        return result;
-    }
-    else
-    {
-        int nnum = (num1.num() * num2.den()) - (num2.num() * num1.den());
-        int nden = (num1.den() * num2.den());
-        RatNum result(nnum, nden);
-        return result;
-    }
-}
-
-RatNum operator*(RatNum& num1, RatNum& num2)
-{
-    int nres = num1.num() * num2.num();
-    int dres = num1.den() * num2.den();
-    RatNum result(nres, dres);
-    return result;
-}
-
-RatNum operator/(RatNum& num1, RatNum& num2)
-{
-    int nres = num1.num() * num2.den();
-    if(num2.num() == 0) error("\nCan't divide because the numerator of the second number is 0");
-    int dres = num1.den() * num2.num();
-    RatNum result(nres, dres);
-    return result;
-}
-
-bool operator==(RatNum& num1, RatNum& num2)
-{
-    if (num1.num() == num2.num() && num1.den() == num2.den()) return true;
     return false;
 }
 
-vector<RatNum>nums;
-
-void select(RatNum& num1, RatNum& num2)
+bool Date::has_31d()
 {
-    int res = 0;
-    RatNum number;
-    while(1)
-    {   
-        cout << endl << "What do you want to do with your numbers (Choose one from the ones listed bellow):" << endl;
-        cout << "1. Addition" << endl
-             << "2. Subtraction" << endl
-             << "3. Multiplication" << endl
-             << "4. Division" << endl
-             << "5. Print Result" << endl
-             << "6. Print Rational as Double" << endl
-             << "7. Check if they are the same" << endl
-             << "8. Input new numbers" << endl
-             << "9. Exit" << endl;
-        if(res == 1) 
-        {
-            cout << "10. Save oparation result" << endl;
-            cout << "Type your answer by typing a number (1 - 10): ";
-        }
-        else 
-        {
-            cout << "Type your answer by typing a number (1 - 9): ";
-        }
-        int sel = 0;
-        cin >> sel;
-        if (sel == 1)
-        {
-            number = num1 + num2;
-            res = 1;
-        }
-        else if(sel == 2)
-        {
-            number = num1 - num2;
-            res = 1;
-        }
-        else if(sel == 3)
-        {
-            number = num1 * num2;
-            res = 1;
-        }  
-        else if(sel == 4)
-        {  
-            number = num1 / num2;
-            res = 1;
-        }  
-        else if(sel == 5)
-        {
-            if (res == 0) error("No oparation has been done to print the result");
-            number.print_r();
-            res = 0;
-        }
-        else if(sel == 6)
-        {
-            cout << endl << "What do you want to print:" << endl
-                 << "1. The First Rational" << endl
-                 << "2. The Second Rational" << endl;
-            if(res == 1) cout << "3. The Result of the Computation" << endl;
-            int print = 0;
-            cout << endl << "Type your choice here (It has to be a number between 1-3): ";
-            cin >> print;
-            if(print == 1)
-            {
-                num1.print_d();
-            }
-            else if(print == 2)
-            {
-                num2.print_d();
-            }
-            else if (print == 3)
-            {
-                number.print_d();
-            }
-            else
-            {
-                error("Incorrect input!");
-            }
+    if (m == jan || m == mar || m == may || m == jul || m == aug || m == octo || m == dece)
+    {
+        return true;
+    }
+    return false;
+}
 
-        }
-        else if(sel == 7)
+bool Date::leap_year()
+{
+    if ((y/4 == 0 && (!(y/100 == 0))) || y/400 == 0)
+    {
+        return true;
+    }
+    return false;
+}
+
+Date::Date(int yy, Month mm, int dd) : y(yy), m(mm), d(dd)
+{
+    if (mm < 1 || mm > 12) error("Invalid Input");
+    if (dd < 1) error("Invalid Input");
+    if (dd > 30 && has_30d()) error("Invalid Input");
+    if (dd > 31 && has_31d()) error("Invalid Input");
+    if (mm == 2 && leap_year() && dd > 29) error("Invalid Input");
+    if (mm == 2 && !leap_year() && dd > 28) error("Invalid Input");
+    if (yy < 1600 || yy > 2299) error("Invalid Input");
+}
+
+int year_code(int year)
+{
+    if (year > 2000) return year - 2000;
+    if (year > 1900) return year - 1900;
+    if (year > 1800) return year - 1800;
+    if (year > 1700) return year - 1700;
+    if (year > 1600) return year - 1600;
+    return -1;
+}
+
+int Date::month_code(int month) 
+{
+    if (month == 1 && !leap_year()) return 1;
+    if (month == 1 && leap_year()) return 4;
+    if (month == 2 && !leap_year()) return 4;
+    if (month == 2 && leap_year()) return 0;
+    if (month == 3) return 4;
+    if (month == 4) return 0;
+    if (month == 5) return 2;
+    if (month == 6) return 5;
+    if (month == 7) return 0;
+    if (month == 8) return 3;
+    if (month == 9) return 6;
+    if (month == 10) return 1;
+    if (month == 11) return 4;
+    if (month == 12) return 6;
+    return -1;
+}
+
+int cent_code(int year)
+{
+    if (year < 1800) return 4;
+    if (year < 1900) return 2;
+    if (year < 2000) return 0;
+    if (year < 2100) return 6;
+    if (year < 2200) return 4;
+    return -1;
+}
+
+Dd Date::day_name()
+{
+    //int day = ((d+m+y+21)%7) + 1;
+    //return Dd(day);
+    
+    int Yy = year_code(y);
+    int Mm = month_code(m);
+    int Cc = cent_code(y);
+    if (Yy == -1 || Mm == -1 || Cc == -1) error("SOmething went wrong!");
+    int sum = Yy/4 + Yy + Mm + Cc + d;
+    sum %= 7;
+    return Dd(sum);
+}
+
+Month operator+(Month& mon, int n) {
+    int temp = mon;
+    temp = temp + n;
+    if(temp > 12)
+    {
+        temp -= 12;
+        return Month(temp);
+    }
+    return Month(temp);
+}
+
+void Date::add_day(int n) 
+{
+    if (n < 0) error("Can't subtract days");
+    if (has_30d()) 
+    {
+        int temp = 0;
+        if (n > 31) error("Too big of a number of days to add!");
+        temp = (d + n)/30;
+        d = (d + n)%30;
+        add_month(temp);
+    }
+    else if (has_31d()) 
+    {
+        int temp = 0;
+        if (n > 30) error("Too big of a number of days to add!");
+        temp = (d + n)/31;
+        d = (d + n)%31;
+        add_month(temp);
+    }
+    else
+    {
+        int temp = 0;
+        if (n > 31) error("Too big of a number of days to add!");
+        if (!leap_year())
         {
-            if (num1 == num2)
-            {
-                cout << endl << "The two rationals are the same!" << endl;
-            }
-            else
-            {
-                cout << endl << "The two rationals aren't the same" << endl;
-            }
-        }
-        else if(sel == 8)
-        {
-            input();
-        }
-        else if(sel == 9)
-        {
-            exit(0);
-        }
-        else if(sel == 10) 
-        {
-            nums.push_back(number);
+            temp = (d + n)/28;
+            d = (d + n)%28;
+            add_month(temp);
         }
         else
         {
-            error("Incorrect input!");
-        }
+            temp = (d + n)/29;
+            d = (d + n)%29;
+            add_month(temp);
+        }  
     }
 }
 
-void input() {
-    cout << endl << "Number No.1:" << endl;
-    cout << "Enter your numerator and denominator:" << endl;
-    cout << ">  ";
-    int num1 = 0;
-    int den1 = 0;
-    cin >> num1;
-    if (cin.fail()) error("Wrong Input");
-    cout << ">  ";
-    cin >> den1;
-    if (cin.fail()) error("Wrong Input");
-    RatNum rnumber1(num1, den1);
-    cout << endl;
-    cout << "Number No.2:" << endl;
-    cout << "Enter your numerator and denominator:" << endl;
-    cout << ">  ";
-    int num2 = 0;
-    int den2 = 0;
-    cin >> num2;
-    if (cin.fail()) error("Wrong Input");
-    cout << ">  ";
-    cin >> den2;
-    if (cin.fail()) error("Wrong Input");
-    RatNum rnumber2(num2, den2);
-    cout << endl;
-    select(rnumber1, rnumber2);
+void Date::add_month(int n) //works
+{
+    int temp1 = m;
+    m = m + n;
+    int temp2 = m;
+    if(temp1 > temp2)
+    {
+        add_year(1);
+    }
 }
 
+void Date::add_year(int n) //works
+{
+    y = y + n;
+}
+
+void Date::next_workday()
+{
+    Dd day_new = day_name();
+    Month month_new = m;
+    int year_new = y;
+    string Dy = " ";
+    if(day_name() == 0 || day_name() == 1 || day_name() == 6)
+    {
+        Dy = "Monday";
+    }
+    else
+    {
+        int temp = day_name();
+        if(d + 1 > 30 && has_30d())
+        {
+            temp++;
+            day_new = Dd(temp);
+            month_new = m + 1;
+            year_new = y;
+        }
+        else if (d + 1 > 31 && has_31d())
+        {
+            temp++;
+            day_new = Dd(temp);
+            month_new = m + 1;
+            year_new = y;
+            if (m == dece) year_new++;
+        }
+        else if ((d + 1) == 28 && !leap_year() || (d + 1) == 29 && leap_year())
+        {
+            temp++;
+            day_new = Dd(temp);
+            month_new = m + 1;
+            year_new = y;
+        }
+        else
+        {
+            temp++;
+            day_new = Dd(temp);
+        }
+        switch (day_new){
+        case 2: Dy = "Monday"; break;
+        case 3: Dy = "Tuesday"; break;
+        case 4: Dy = "Wednesday"; break;
+        case 5: Dy = "Thursday"; break;
+        case 6: Dy = "Friday"; break;
+        }
+        if (day_new == 1 || day_new == 0) error("Something went wrong...");
+    }
+    string Mm = " ";
+    switch (month_new) {
+    case 1: Mm = "January"; break;
+    case 2: Mm = "February"; break;
+    case 3: Mm = "March"; break;
+    case 4: Mm = "April"; break;
+    case 5: Mm = "May"; break;
+    case 6: Mm = "June"; break;
+    case 7: Mm = "July"; break;
+    case 8: Mm = "August"; break;
+    case 9: Mm = "September"; break;
+    case 10: Mm = "Octomber"; break;
+    case 11: Mm = "November"; break;
+    case 12: Mm = "December"; break;
+    }
+    cout << "The next workday is: " << Dy
+         << " " << Mm 
+         << " " << year_new <<  endl;
+}
+
+void Date::week_of_year()
+{
+    int n = d/7 + (m - 1)*4;
+    int sum = 0;
+    if (d%7 != 0) sum += d%7;
+    for(int i = m; i; i--)
+    {
+        if (i == 4 || i == 6 || i == 9 || i == 11)
+        {
+            sum += 2;
+        }
+        else if(i != 2)
+        {
+            sum += 3;
+        }
+    }
+    n = n + sum/7 + 1;
+    cout << "It is week " << n << " of the year!" << endl;
+}
 
 int main()
 {
-    try
-    {
-        input();
+    try{
+        Date today(1643, jan, 4);
+        today.week_of_year();
+        today.next_workday();
+        return 0;
     }
     catch(exception& e)
     {
@@ -245,6 +291,6 @@ int main()
     }
     catch(...)
     {
-        cerr << "Oops something went wrong!" << endl;
+        cerr << "Something went wrong and the app had to be shut down.." << endl;
     }
 }
--- README.md
+++ README.md
@@ -1,4 +1,15 @@
-## CPP-Console-Applications
-# Some small fun console applications in C++
+# CPP-Console-Applications
+## Some small fun console applications in C++
+
 The main purpose of this repository is to help myself and anyone<br/>
 out there understand c++ a little bit more.<br/>
+
+### Programs
+
+1. **RatNum.cpp**<br/>
+Takes as input two integers for each of the two rationals that the computation in going to be done to.<br/>
+The available computations are:
+- Addition
+- Subtraction
+
+
